b358ce8eb031cd6d4747c9b0176888c570a15282
jry016 <hjungbin99@naver.com>
2023-07-07 18:33:03 +0900

    sync

--- Sideways Sorting.py
+++ Sideways Sorting.py
@@ -1,6 +1,6 @@
 # KATTIS CODING TEST PROBLEMS
-# Sideways Sorting
-# https://open.kattis.com/problems/sidewayssorting
+# MIA
+# https://nus.kattis.com/courses/CS2040/CS2040_S4_AY1718/assignments/f3bh9x/problems/mia
 
 # boolean continueLoop = true;
 #         while (continueLoop) {
@@ -19,10 +19,40 @@
 #                     }
 #                 }
 
-# Getting Input
-continueLoop = True
 
-def getInput(inputs):
-    while(continueLoop):
-        
+def Mia(input):
+    double_score = {
+        "66" : 72, 
+        "55" : 71, 
+        "44" : 70, 
+        "33" : 69, 
+        "22" : 68, 
+        "11" : 67
+    }
     
+    if (input in ["12", "21"]):
+        return 73
+
+    elif (input in double_score):
+        return double_score[input]
+
+    else:
+        return int(sorted(input, reverse = True))
+
+s0, s1, r0, r1 = input().split()
+while(True):
+    #s0, s1, r0, r1 = input().split() # string
+                                     # map(int, input().split())
+    if(s0 == '0'):
+        break
+    
+    p1_score = Mia(s0 + s1)
+    p2_score = Mia(r0 + r1)
+    
+    if(p1_score > p2_score):
+        print("Player 1 wins.")
+    elif(p1_score < p2_score):
+        print("Player 2 wins.")
+    else:
+        print("Tie.")
+        
