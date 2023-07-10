03ff608d217a3c4a917d36c4d4d276911cf1f928
jry016 <hjungbin99@naver.com>
2023-07-10 14:47:13 +0900

    Updated

--- [Kattis]SidewaysSorting.py
+++ [Kattis]SidewaysSorting.py
@@ -2,4 +2,29 @@
 # Sideways Sorting
 # https://open.kattis.com/problems/sidewayssorting
 
+inputs = []
+results = []
 
+while True:
+    row, col = map(int, input().split())
+    if row == 0 and col == 0:
+        break
+    
+    for _ in range(row):
+        line = input().strip() # remove any leading or trailing whitespace characters
+        inputs.append(line)
+
+    # Transpose the grid
+    transposed_grid = ["".join(col) for col in zip(*grid)]
+
+    # Sort the transposed grid lexicographically
+    sorted_grid = sorted(transposed_grid)
+
+    # Transpose the sorted grid back
+    result = ["".join(row) for row in zip(*sorted_grid)]
+
+    # Print the result
+    for line in result:
+        print(line)
+     
+    print()  # Add a blank line between outputs
