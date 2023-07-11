498e806c3540910bcfde62fcf47233ffcc9cb6b5
jry016 <hjungbin99@naver.com>
2023-07-11 18:26:40 +0900

    final_version

---
+++ [Kattis]IntegerLists.py
@@ -0,0 +1,27 @@
+# KATTIS
+# Integer Lists
+# https://open.kattis.com/problems/integerlists
+
+def Reverse(arr):
+    arr = arr[::-1] # Reverse the list
+                    # arr.reverse()
+    
+def Remove(arr):
+    arr = arr[1:] # Remove the first element
+    
+inputs = []    
+while True:
+    line = input()
+    cnt = 0
+    inputs.append(line)
+    
+    # Terminating Condition
+    if(cnt == inputs[0]):
+        break
+    
+    for i in range(i+1, inputs[0]*4 + 1):
+        
+    
+    cnt += 1
+    
+    
