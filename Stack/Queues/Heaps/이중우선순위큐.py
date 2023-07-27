4b46b195ee8616470be646c054c884268adff845
jry016 <hjungbin99@naver.com>
2023-07-27 10:31:09 +0900

    reverted

--- Stack/Queues/Heaps/이중우선순위큐.py
+++ Stack/Queues/Heaps/이중우선순위큐.py
@@ -2,22 +2,43 @@
 # 이중우선순위큐
 # https://school.programmers.co.kr/learn/courses/30/lessons/42628
 
+from heapq import *
+
 def solution(operations):
-    answer = []
-    return answer
+    op = [[a, int(b)] for a, b in [pair.split() for pair in operations]]
+    heap = []
+    
+    for i in op:
+        if i[0] == 'I':
+            heappush(heap, i[1])
+        
+        elif i[0] == 'D':
+            if len(heap) > 0:
+                # Remove Max Value from Heap
+                if i[1] == 1:
+                    heap.pop(heap.index(nlargest(1, heap)[0]))  
+                # Remove Min Value from Heap
+                else:
+                    heappop(heap)
+    if not heap:
+        return [0, 0]
+    else:
+        return [nlargest(1,heap)[0], heap[0]]  
+        
+# nlargest(n, heap) 함수는 n개의 가장 큰 값들로 이루어진 리스트를 반환한다
 
 # Test Case
-# operations = ["I 16", "I -5643", "D -1", "D 1", "D 1", "I 123", "D -1"]
-# print(solution(operations)) #[0, 0]
+operations = ["I 16", "I -5643", "D -1", "D 1", "D 1", "I 123", "D -1"]
+print(solution(operations)) #[0, 0]
 
-# operations2 = ["I -45", "I 653", "D 1", "I -642", "I 45", "I 97", "D 1", "D -1", "I 333"]
-# print(solution(operations2)) #[333, -45]
+operations2 = ["I -45", "I 653", "D 1", "I -642", "I 45", "I 97", "D 1", "D -1", "I 333"]
+print(solution(operations2)) #[333, -45]
 
 # ### Attempt 1: Runtime Error
 # from heapq import *
 
 # def solution(operations):
-#     op = [[a,int(b)] for a, b in [pair.split() for pair in operations]]
+#     op = [[a, int(b)] for a, b in [pair.split() for pair in operations]]
     
 #     heap = []
 #     for i in op:
