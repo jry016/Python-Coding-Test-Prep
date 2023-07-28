86ce76a601cb9dea2e3e9b14fbddaf8d99fa6055
jry016 <hjungbin99@naver.com>
2023-07-28 17:17:23 +0900

    started

---
+++ Simple Sorts/가장 큰 수.py
@@ -0,0 +1,15 @@
+# 가장 큰 수
+# https://school.programmers.co.kr/learn/courses/30/lessons/42746
+
+# search for largest number in the largest digit
+
+def solution(numbers):
+    answer = ''
+    return answer
+
+a = ['666', '232222', '9']
+print(sorted(a, reverse = True))
+
+b = ['3', '30', '34', '5','9']	
+print(sorted(b, reverse = True))
+
